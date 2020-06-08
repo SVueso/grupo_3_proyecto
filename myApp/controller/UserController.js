d507612e043951a488d258525402a0c76ab6a9d9
Mateo <barbatomateo@gmail.com>
2020-06-08 14:49:30 -0300

    Hello subo avances en controller y partial del head, falta anadir un script que hace que funcione el partial del head, todavia no esta funcionando

---
+++ myApp/controller/UserController.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+let dbpath = path.join(__dirname, `../ASIGNARRUTA`);
+let db = fs.readFileSync(filePath, 'utf-8');
+
+CONST userController = {
+
+}
+
+
+
+
+
+
+
+module.exports = userController
