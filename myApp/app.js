f5ef2b38bc1da5e097476960e91540f0e1ffcb80
Mateo <barbatomateo@gmail.com>
2020-06-11 13:21:31 -0300

    Cambio nombre ruta index a ruta producto

--- myApp/app.js
+++ myApp/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index.js');
+var productoRouter = require('./routes/producto.js');
 var usersRouter = require('./routes/users.js');
 
 
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',indexRouter);
+app.use('/',productoRouter);
 app.use('/user',usersRouter);
 
 
